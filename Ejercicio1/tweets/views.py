0e91cc0b021d5c4b8ee6fe8c4f6da1c4cff65b8c
Rubén Fernández <rubick@gmail.com>
2015-04-14 14:11:30 +0200

    Tema 2 Ejercicio 1 Added urls nick and detail

--- Ejercicio1/tweets/views.py
+++ Ejercicio1/tweets/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from tweets.models import Tweet
+def index(request):
+    tweet_list = Tweet.objects.order_by('-pub_date')[:10]
+    lista = []
+    for tweet in tweet_list:
+        lista.append(tweet.tweet_text + ' por ' + tweet.user.nick + '<br/>')
+    output = ''.join(lista)
+    return HttpResponse(output)
+
+def user_tweets(request, nick):
+    return HttpResponse('Este es el timeline de %s' % nick)
+
+def tweet_detail(request, tweet_id):
+    return HttpResponse('Este es el tweet con id %s' % tweet_id)
