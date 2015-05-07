f995609cf2769ab7ab65b1402bab62528ceff465
Rubén Fernández <rubick@gmail.com>
2015-05-07 17:38:12 +0200

    Ejercicio 5. Added Class Views

--- Ejercicio/Noticias/urls.py
+++ Ejercicio/Noticias/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns,url
 from Noticias import views
 urlpatterns = patterns('',
         url(r'^addescritorcbv/', views.AddEscritor.as_view()),
+        url(r'^listaescritores/', views.EscritoresList.as_view()),
     )
--- Ejercicio/Noticias/views.py
+++ Ejercicio/Noticias/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
-from Noticias.forms import EscritorForm
-
+from Noticias.forms import *
+from Noticias.models import *
+from django.views.generic import ListView
+from django.db.models import Count
 class AddEscritor (View):
     def get (self, request):
         form = EscritorForm()
@@ -15,3 +17,16 @@ class AddEscritor (View):
            #TODO return HttpResponseRedirect ("/noticias/welcome")
             return HttpResponse('<p class="bg-success">Usuario registrado!</p>')
         return render (request, 'add_escritor.html', {'form':form})
+
+class EscritoresList(ListView):
+    model = Escritor
+    template_name = 'escritores_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EscritoresList, self).get_context_data(**kwargs)
+        noticias = Noticia.objects.values('autor').annotate(dcount=Count('autor'))
+        noticias_bien = {}
+        for elemento in noticias:
+            noticias_bien[elemento["autor"]] = elemento["dcount"]
+            context["noticias"] = noticias_bien
+            return context
